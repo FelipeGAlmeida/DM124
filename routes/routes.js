eebcff35c5bf341518644626176c9ba2b3f5196c
Felipe G. Almeida <felipegalmeida@outlook.com>
2019-09-03 10:54:42 -0300

    New Routes - New routes added

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (request, response) => {
- response.status(200).json({
-   message: 'got the /test message !'
- });
+router.post('/test', (request, response) => {
+    response.status(200).json({
+        message: 'post to the /test path !'
+    });
 });
+
+router.get('/test', (request, response) => {
+    response.status(200).json({
+        message: 'got the /test message !'
+    });
+});
+
+router.get('/test/:taskId', (request, response) => {
+    const id = request.params.taskId; //Get the parameter passed
+    response.status(200).json({
+        message: `Task with ID = ${id} was fetched`
+    });
+});
+
+router.patch('/test/:taskId', (request, response) => {
+    const id = request.params.taskId; //Get the parameter passed
+    response.status(200).json({
+        message: `Task with ID = ${id} was updated`
+    });
+});
+
+router.delete('/test/:taskId', (request, response) => {
+    const id = request.params.taskId; //Get the parameter passed
+    response.status(200).json({
+        message: `Task with ID = ${id} was deleted`
+    });
+});
+   
 
 module.exports = router;
